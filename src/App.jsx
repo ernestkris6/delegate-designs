5c6bf4e7f4b99000974901f738cc314f351c69a3
ERNEST CHRIS <ernestkris16@gmail.com>
2026-05-30 22:37:04 +0100

    defining routess

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Error from "./pages/Error";
 
 export default function App() {
   return (
-    <div className="text-3xl text-center pt-24">HELLO WORLD!!!!!</div>
+    <>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/"  element={<Home />}/>
+            <Route path="about" element={<About />} />
+            <Route path="services" element={<Services />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="portfolio" element={<Portfolio />} />
+            <Route path="*" element={<Error />} />
+            
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+   <div className="text-3xl text-center pt-24">
+      HELLO WORLD!!!!!
+   </div>
+  )
+}
